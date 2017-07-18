5a533e936a65327f98a57808631166d4d76905ca
Clement LAFLEUR <clafleur@talentsoft.com>
2017-07-18 17:54:30 +0200

    add new test

--- kata06-anagrams/AnagramsLibTests/AnagramsTests.cs
+++ kata06-anagrams/AnagramsLibTests/AnagramsTests.cs
@@ -2,6 +2,7 @@
 {
     using AnagramsLib;
     using NUnit.Framework;
+    using System.Collections.Generic;
 
     [TestFixture]
     public class AnagramsTests
@@ -26,6 +27,16 @@
 
             var result = Anagrams.GetAnagramHash(word);
 
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void TestBuildAnagramsCollection_ForAListOfAnagramWords_ReturnsOneAnagramCollection()
+        {
+            var words = new List<string>(new[] { "word", "dwor", "rodw" });
+            IList<IList<string>> expectedResult = new List<IList<string>>(new[] { new List<string>(new[] { "word", "dwor", "rodw" }) });
+
+            var result = Anagrams.BuildAnagramsCollection(words);
             Assert.That(result, Is.EqualTo(expectedResult));
         }
 
